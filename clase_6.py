452ebe70fbc22efe2f63291a2360793efa00382b
dcsoft-ux <cdazaparra@gmail.com>
2025-10-24 21:01:16 -0500

    try3

--- clase_6.py
+++ clase_6.py
@@ -1,6 +1,21 @@
-class Pajaro:
-    pass
+# class Pajaro:
+#     pass
+# mi_pajaro = Pajaro()
+# print(mi_pajaro)
+# print(type(mi_pajaro))
 
-mi_pajaro = Pajaro()
+# Atributos
+class Pajaro:
+    alas = 2
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+
+color = input("Escribe el color del pájaro: ")
+especie = input("Escribe la especie del pájaro: ")
+mi_pajaro = Pajaro(color, especie)
 print(mi_pajaro)
 print(type(mi_pajaro))
+print("Color:", mi_pajaro.color)
+print("Especie:", mi_pajaro.especie)
+print("Alas:", mi_pajaro.alas)
